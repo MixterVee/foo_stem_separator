2fac82ba29452c9a7d8ee6149606de335cfe7046
MixterVee <mixtervandal@gmail.com>
2026-08-18 18:13:03 -0400

    Add cache settings test build wrapper

---
+++ build-cache-settings-test.ps1
@@ -0,0 +1,29 @@
+param(
+    [Parameter(Mandatory=$true)] [string]$SdkRoot,
+    [Parameter(Mandatory=$true)] [string]$RepoRoot,
+    [Parameter(Mandatory=$true)] [string]$WtlInclude,
+    [Parameter(Mandatory=$true)] [string]$AtlInclude
+)
+
+$ErrorActionPreference = "Stop"
+
+Push-Location $RepoRoot
+try {
+    python .\patch-cache-settings.py
+    if ($LASTEXITCODE -ne 0) {
+        throw "Cache settings source patch failed."
+    }
+
+    & .\build-ci-v12.ps1 `
+        -SdkRoot $SdkRoot `
+        -RepoRoot $RepoRoot `
+        -WtlInclude $WtlInclude `
+        -AtlInclude $AtlInclude
+
+    if ($LASTEXITCODE -ne 0) {
+        throw "Stem Separator build failed."
+    }
+}
+finally {
+    Pop-Location
+}
